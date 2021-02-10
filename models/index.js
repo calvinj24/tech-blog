e02be09aaf0ff688fbe59fd7a61a97eb5707cb4c
Calvin Johnson <calvinjohnson@Calvins-MBP-7.attlocal.net>
2021-02-09 21:07:13 -0600

    add comment model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
   foreignKey: 'user_id'
@@ -8,6 +9,25 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
   foreignKey: 'user_id',
   onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
 });
 
 module.exports = { User, Post }
