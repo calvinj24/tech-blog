4c74b4bf6993bd5c1196fd3cf2d94f63ef17e457
Calvin Johnson <calvinjohnson@Calvins-MBP-7.attlocal.net>
2021-02-10 16:11:44 -0600

    add dashboad view

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -12,7 +12,7 @@ router.get('/', withAuth, (req, res) => {
     },
     attributes: [
       'id',
-      'post_url',
+      'post_content',
       'title',
       'created_at'
     ],
--- models/User.js
+++ models/User.js
@@ -2,12 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-// create our User model
 class User extends Model {
-
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
-// create fields/columns for User model
 User.init(
   {
     id: {
